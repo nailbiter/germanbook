add2a5bf6fff68045491f2c39f91faf14680e1c3
Alex Leontiev <alozz1991@gmail.com>
2017-04-13 21:17:45 +0900

    list files

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QDir>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
@@ -8,5 +10,23 @@ int main(int argc, char *argv[])
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
 
-    return app.exec();
+    QDir dir("../germanbook/hungerkunstler");
+    dir.setFilter(QDir::Files | QDir::Hidden | QDir::NoSymLinks);
+    dir.setSorting(QDir::Size | QDir::Reversed);
+
+    QFileInfoList list = dir.entryInfoList();
+    std::cout << "     Bytes Filename" << std::endl;
+    for (int i = 0; i < list.size(); ++i) {
+        QFileInfo fileInfo = list.at(i);
+        std::cout << qPrintable(QString("%1 %2").arg(fileInfo.size(), 10)
+                                                .arg(fileInfo.fileName()));
+        std::cout << std::endl;
+    }
+    return 0;
+
+    //return app.exec();
 }
+/*QMediaPlayer* player = new QMediaPlayer;
+player->setMedia(QUrl::fromLocalFile("/Users/me/Music/coolsong.mp3"));
+player->setVolume(50);
+player->play();*/
